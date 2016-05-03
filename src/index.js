05a6b9b981aa217636b11fc2e35e6aca2968aaaa
jslopeza <jasonlopeza95@gmail.com>
2016-05-03 19:37:15 -0400

    Adds react hot module loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import config from '../config';
 import SearchBar from './components/search_bar';
-const App = () => {
-	return <SearchBar />;
+
+class App extends Component {
+	render() {
+		return (
+			<SearchBar />
+		)
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
